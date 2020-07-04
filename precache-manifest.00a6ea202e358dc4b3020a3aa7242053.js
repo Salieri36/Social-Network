15f0b41961841ea2ed974bf38a56d30d9fa33597
Salieri36 <36salieri@gmail.com>
2020-07-04 23:35:27 +0300

    Updates

---
+++ precache-manifest.00a6ea202e358dc4b3020a3aa7242053.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f101862c7ec52a2f9d1e5002069bd229",
+    "url": "/social-network/index.html"
+  },
+  {
+    "revision": "23873e61f3bdd16fb1c8",
+    "url": "/social-network/static/css/main.1b24c442.chunk.css"
+  },
+  {
+    "revision": "0919986ae0f47aa54071",
+    "url": "/social-network/static/js/2.18385d7c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/social-network/static/js/2.18385d7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23873e61f3bdd16fb1c8",
+    "url": "/social-network/static/js/main.098cdacc.chunk.js"
+  },
+  {
+    "revision": "1b3d5d9bf4ccf1d93a12",
+    "url": "/social-network/static/js/runtime-main.07757ca8.js"
+  },
+  {
+    "revision": "8be1620010705154fe07eff863584552",
+    "url": "/social-network/static/media/Lineage_OS_Logo.8be16200.png"
+  },
+  {
+    "revision": "ce8e7ab80c30d435d1557087b2a130dc",
+    "url": "/social-network/static/media/my-avatar.ce8e7ab8.png"
+  }
+]);
